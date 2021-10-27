f46588d5bd831a2c3cf66dae8767d10442e5f113
gmilazzo <milazzo.ga@gmail.com>
2021-10-27 06:54:23 +0200

    fix

--- src/main/java/com/enginious/fjwt/config/EnableFjwt.java
+++ src/main/java/com/enginious/fjwt/config/EnableFjwt.java
@@ -7,6 +7,26 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Add this annotation to an {@link org.springframework.context.annotation.Configuration} or
+ * {@link org.springframework.boot.autoconfigure.SpringBootApplication} class to have
+ * the Fjwt configured, for example:
+ * <pre class="code">
+ * &#064;EnableFjwt
+ * &#064;Configuration
+ * public class MySecurityConfiguration{
+ * ...
+ * }
+ * </pre>
+ * or
+ * <pre class="code">
+ * &#064;EnableFjwt
+ * &#064;SpringBootApplication
+ * public class MySpringBootApplication{
+ * ...
+ * }
+ * </pre>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Import(FjwtConfig.class)
