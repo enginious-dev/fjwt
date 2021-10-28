70e50a05f2783ed95927df637a214af2bd65bb2c
gmilazzo <milazzo.ga@gmail.com>
2021-10-28 14:47:23 +0200

    fix clock

--- src/main/java/com/enginious/fjwt/core/FjwtSecurityConfig.java
+++ src/main/java/com/enginious/fjwt/core/FjwtSecurityConfig.java
@@ -1,6 +1,10 @@
 package com.enginious.fjwt.core;
 
+import java.time.Clock;
+import java.time.ZoneId;
 import java.util.Collections;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,5 +43,17 @@ public class FjwtSecurityConfig {
   @ConditionalOnMissingBean(UserDetailsService.class)
   public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
     return s -> new User(s, passwordEncoder.encode(s), Collections.emptyList());
+  }
+
+  /**
+   * register the default {@link Clock}
+   *
+   * @param zoneId the zone id
+   * @return the default clock bean
+   */
+  @Bean
+  @ConditionalOnMissingBean(Clock.class)
+  public Clock clock(@Value("${fjwt.zoneId:}") String zoneId) {
+    return Clock.system(StringUtils.isBlank(zoneId) ? ZoneId.systemDefault() : ZoneId.of(zoneId));
   }
 }
